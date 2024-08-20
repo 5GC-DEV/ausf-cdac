b01a72a133bc55163dc193794da712726f9e67e5
ashithacdac <159995090+ashithacdac@users.noreply.github.com>
2024-08-20 10:07:52 +0530

    Update ue_authentication.go

--- producer/ue_authentication.go
+++ producer/ue_authentication.go
@@ -33,7 +33,7 @@ const AV_GENERATION_PROBLEM_ERROR = "AV_GENERATION_PROBLEM"
 
 // Generates a random int between 0 and 255
 func GenerateRandomNumber() (uint8, error) {
-	max := big.NewInt(256)
+	maximum := big.NewInt(256)
 	randomNumber, err := rand.Int(rand.Reader, max)
 	if err != nil {
 		return 0, err
