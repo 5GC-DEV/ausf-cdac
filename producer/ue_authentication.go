7adbb4bd18d89895cde6159738d6ce49338e5bf5
ReshmaJS <reshma.js@cdac.in>
2025-06-19 14:19:36 +0530

    status on ausf ue auth changed

--- producer/ue_authentication.go
+++ producer/ue_authentication.go
@@ -153,7 +153,7 @@ func UeAuthPostRequestProcedure(updateAuthenticationInfo models.AuthenticationIn
 		} else {
 			problemDetails.Cause = UPSTREAM_SERVER_ERROR
 		}
-		problemDetails.Status = http.StatusInternalServerError
+		problemDetails.Status = http.StatusNotFound
 		logger.UeAuthPostLog.Infoln("=======HTTP Status Code:========= ", problemDetails.Status)
 		return nil, "", &problemDetails
 	}
